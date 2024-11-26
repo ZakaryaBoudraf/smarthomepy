c31bbb65fa9eaf3dd259a491b39ac109f4aacd57
Zakarya <52080782+ZakaryaBoudraf@users.noreply.github.com>
2024-11-26 16:09:58 +0100

    Red Phase 1 - US1

--- test/test_smart_room.py
+++ test/test_smart_room.py
@@ -11,6 +11,7 @@ from mock.senseair_s8 import SenseairS8
 class TestSmartRoom(unittest.TestCase):
 
     @patch.object(GPIO, "input")
-    def test_something(self, mock_object: Mock):
-        # This is an example of test where I want to mock the GPIO.input() function
-        pass
+    def test_check_room_occupancy_true(self, mock_infrared_sensor: Mock):
+        system = SmartRoom()
+        mock_infrared_sensor.return_value = True
+        self.assertTrue(system.check_room_occupancy())
